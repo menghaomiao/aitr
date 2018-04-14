265335aea9bf6e8083adf439b133d00d18a6f400
menghaomiao <haomiaomeng@126.com>
2018-04-13 23:30:23 -0400

    adjust diagonal of kernel

--- R/itrFit.R
+++ R/itrFit.R
@@ -15,13 +15,14 @@ itrFit=function(x, a, y, p=1/k, s=1.2, method='svm', kernel='linear', epsilon=1/
  sminus=s-1
  x=as.matrix(x)
  if (kernel=='linear') {
-  K=x%*%t(x)
+  ker=x%*%t(x)
  } else if (kernel=='gaussian') {
   d=as.matrix(dist(x))
-  K=exp(-epsilon*d^2)
+  ker=exp(-epsilon*d^2)
  } else if (kernel=='polynomial') {
-  K=(1+x%*%t(x))^d
+  ker=(1+x%*%t(x))^d
  } else stop('kernel must be one of the followings: linear, gaussian, polynomial.')
+ K=ker+diag(n)*0.01*mean(diag(ker))
  if (method=='svm') {
   K=K+1
   WWK=ind*K
@@ -34,11 +35,11 @@ itrFit=function(x, a, y, p=1/k, s=1.2, method='svm', kernel='linear', epsilon=1/
    for (i in 1:5) {
     id=folds==i
     theta_s_gamma=svmfit_C(WWK[!id, !id], diagK[!id], w[!id], sminus, lambda)
-    inner[id, ]=get_inner(theta_s_gamma, K[id, !id], W_aW[!id, ])
+    inner[id, ]=get_inner(theta_s_gamma, ker[id, !id], W_aW[!id, ])
    }
   } else {
    theta_s_gamma=svmfit_C(WWK, diagK, w, sminus, lambda)
-   inner=get_inner(theta_s_gamma, K, W_aW)
+   inner=get_inner(theta_s_gamma, ker, W_aW)
   }
   dim(inner)=c(n, k, m)
  } else if (method=='dwd') {
@@ -52,13 +53,13 @@ itrFit=function(x, a, y, p=1/k, s=1.2, method='svm', kernel='linear', epsilon=1/
     id=folds==i
     A=dwdfit_C(WWK[!id, !id], K[!id, !id], W[!id, ], w[!id], sminus, lambda)
     for (j in 1:m) {
-     inner[id, , j]=cbind(1, K[id, !id])%*%A[, , j]%*%Wbasis
+     inner[id, , j]=cbind(1, ker[id, !id])%*%A[, , j]%*%Wbasis
     }
    }
   } else {
    A=dwdfit_C(WWK, K, W, w, sminus, lambda)
    for (j in 1:m) {
-    inner[, , j]=cbind(1, K)%*%A[,  ,j]%*%Wbasis
+    inner[, , j]=cbind(1, ker)%*%A[,  ,j]%*%Wbasis
    }
   }
  } else stop('current version only supports svm and dwd loss.')
@@ -71,22 +72,20 @@ itrFit=function(x, a, y, p=1/k, s=1.2, method='svm', kernel='linear', epsilon=1/
  if (opt==1 | opt==m) warning('The lambda may not be optimal. Another range may be considered.')
  optlambda=lambda[opt]
  optinner=inner[, , opt]
- res=list(obj_value=c(itr=obj[opt]), optlambda=optlambda, s=s, level=level.name, x=x, a=a, y=y)
+ res=list(obj_value=c(itr=obj[opt]), optlambda=optlambda, s=s, level=level.name, x=x, a=a, y=y, kernel=ker)
  if (method=='svm') {
   if (cv) {
    theta_s_gamma=svmfit_C(WWK, diagK, w, sminus, optlambda)
-   inner=get_inner(theta_s_gamma, K, W_aW)
+   inner=get_inner(theta_s_gamma, ker, W_aW)
   } else theta_s_gamma=theta_s_gamma[, opt]
   res$theta_s_gamma=as.numeric(theta_s_gamma)
-  res$kernel=K-1
   class(res)=c('itrfit.svm', 'itrfit')
  } else {
   if (cv) {
    A=dwdfit_C(WWK, K, W, w, sminus, lambda[1:opt])
-   inner=cbind(1, K)%*%A%*%Wbasis
+   inner=cbind(1, ker)%*%A%*%Wbasis
   } else A=A[, , opt]
   res$coef=A
-  res$kernel=K
   class(res)=c('itrfit.dwd', 'itrfit')
  }
  attr(res$kernel, 'type')=kernel
